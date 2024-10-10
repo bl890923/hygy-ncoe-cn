ee6410c34d4f3297f58b350a35dc2651111ef16f
bl890923 <89bl@163.com>
2024-10-11 00:53:19 +0800

    Add files via upload

---
+++ static/js/ey_global.js
@@ -0,0 +1,71 @@
+function ShopAddCart1625194556(aid, spec_value_id, num, rootDir)
+{
+    rootDir = rootDir ? rootDir : '';
+    $.ajax({
+        url: rootDir + '/index.php?m=user&c=Shop&a=shop_add_cart&_ajax=1',
+        data: {
+            aid: aid,
+            num: num,
+            spec_value_id: spec_value_id
+        },
+        type: 'post',
+        dataType: 'json',
+        success: function(res)
+        {
+            if (1 == res.code)
+            {
+                window.location.href = res.url
+            }
+            else
+            {
+                if (-1 == res.data.code)
+                {
+                    layer.msg(res.msg, { time: time })
+                }
+                else
+                {
+                    window.location.href = res.url
+                }
+            }
+        }
+    })
+}
+function ey_setCookies(name, value, time)
+{
+    var cookieString = name + "=" + escape(value) + ";";
+    if (time != 0)
+    {
+        var Times = new Date;
+        Times.setTime(Times.getTime() + time);
+        cookieString += "expires=" + Times.toGMTString() + ";"
+    }
+    document.cookie = cookieString + "path=/"
+}
+function getCookie(c_name)
+{
+    if (document.cookie.length > 0)
+    {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1)
+        {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1)
+                c_end = document.cookie.length;
+            return unescape(document.cookie.substring(c_start, c_end))
+        }
+    }
+    return ""
+}
+function ey_getCookie(c_name)
+{
+    return getCookie(c_name)
+}
+function getQueryString(name)
+{
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null)
+        return unescape(r[2]);
+    return null
+}
